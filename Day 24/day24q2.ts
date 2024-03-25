b1bd82fdb42cd4da64ade617250c10cfa8afe19e
Amna Sadia Korai <murtazaamna117@gmail.com>
2024-03-25 03:34:16 -0700

    Day 24 of 100 days of TypeScript Challenge.

---
+++ Day 24/day24q2.ts
@@ -0,0 +1,12 @@
+//Day 24 Question No. 2.
+//Question 71: Compare let and const: Create two examples where let allows reassignment but const does not. Try to reassign a const-declared variable and catch the error.
+let age = 18;
+age = 19; 
+console.log(age); 
+
+const gameName = "Subway Surfers";
+try {
+    // gameName = "MineCraft"; // This line will cause an error
+} catch (error) {
+    console.log("Error: Can't reassign a `const`-declared variable.");
+}
---
+++ Day 24/day24q3.ts
@@ -0,0 +1,18 @@
+//Day 24 Question No. 3.
+//Question 72: Block Scope with let and const: Demonstrate block scope by creating a code block with {} that uses both let and const. Show how variables declared inside the block are not accessible outside of it.
+{
+    let blockLet = "visible inside the block";
+    const blockConst = "also only inside the block";
+    console.log(blockLet); // Works fine here
+    console.log(blockConst); // Also works fine here
+}
+try {
+    // console.log(blockLet); 
+} catch (error) {
+    console.log("`blockLet` is not accessible outside the block.");
+}
+try {
+    // console.log(blockConst); 
+} catch (error) {
+    console.log("`blockConst` is not accessible outside the block.");
+}
