58815360f58fbac2c75544a59cb591eaf3c1ad27
Amna Sadia Korai <murtazaamna117@gmail.com>
2024-03-31 15:18:06 +0500

    Day 27 of 100 days of  TypeScript Challenge.

---
+++ Day 27/day27q3.js
@@ -0,0 +1,15 @@
+"use strict";
+//Question 81: Iterating Over Object Properties: Write a function that takes an object as an argument and logs all of its properties and values.
+function logObjectProperties(obj) {
+    for (let prop in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+            console.log(`${prop}: ${obj[prop]}`);
+        }
+    }
+}
+const sampleObject = {
+    name: 'Faran',
+    age: 26,
+    city: 'Karachi'
+};
+logObjectProperties(sampleObject);
