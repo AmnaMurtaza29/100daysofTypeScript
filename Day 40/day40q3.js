6f0751153c4ee7b1fa3c91c05bec2e3ce06466b4
Amna Sadia Korai <murtazaamna117@gmail.com>
2024-04-17 16:56:51 +0500

    Day 40 of 100 days of TypeScipt Coding Challenge.

---
+++ Day 40/day40q3.js
@@ -0,0 +1,6 @@
+"use strict";
+//Question 120: Use a for...of loop to iterate through an array of your favorite movies and log each one.
+let favoriteMovies = ["Little Women", "The Silent Patient", "Six of Crows", "Imran Seris"];
+for (let movie of favoriteMovies) {
+    console.log(movie);
+}
---
+++ Day 40/day40q3.ts
@@ -0,0 +1,6 @@
+//Question 120: Use a for...of loop to iterate through an array of your favorite movies and log each one.
+let favoriteMovies: string[] = ["Little Women", "The Silent Patient", "Six of Crows", "Imran Seris"];
+
+for (let movie of favoriteMovies) {
+    console.log(movie);
+}
