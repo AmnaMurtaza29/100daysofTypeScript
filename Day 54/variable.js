78bc3a17a2f0e5366de0175bb59acb6f21847fda
Amna Sadia Korai <murtazaamna117@gmail.com>
2024-04-24 12:13:51 +0500

    Day-54 of 100-Days-Of-Code Challenge

---
+++ Day 54/variable.js
@@ -0,0 +1,16 @@
+//Declare two variables: admin and name.
+
+let admin;
+let Name;
+
+Name = "John";
+admin = Name;
+
+alert(admin)
+
+//Create a variable with the name of our planet. How would you name such a variable?
+//Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+let planet = "Earth";
+
+let currentVisitor = "Huda";
