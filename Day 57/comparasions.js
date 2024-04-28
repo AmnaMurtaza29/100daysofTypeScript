68c8667df75513014f81c6dae1975a2f2d3469ee
Amna Sadia Korai <murtazaamna117@gmail.com>
2024-04-28 13:57:05 +0500

    Day-57 of 100-Days-Of-Code Challenge

---
+++ Day 57/comparasions.js
@@ -0,0 +1,8 @@
+console.log(5 > 4); 
+console.log("apple" > "pineapple");
+console.log("2" > "12");
+console.log(undefined == null);
+console.log(undefined === null);
+console.log(null == "\n0\n");
+console.log(null === +"\n0\n");
+let result = (a + b < 4) ? 'Below' : 'Over';
---
+++ Day 57/conditional.js
@@ -0,0 +1,6 @@
+let result = (a + b < 4) ? 'Below' : 'Over';
+let message =
+  (login == 'Employee') ? 'Hello' :
+  (login == 'Director') ? 'Greetings' :
+  (login == '') ? 'No login' :
+  '';
