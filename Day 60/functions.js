cb95ee09e42ab53f0fcda0f1b2e6c65c4b1fc6cb
Amna Sadia Korai <murtazaamna117@gmail.com>
2024-05-01 21:41:36 +0500

    Day-60 of 100-Days-Of-Code Challenge

---
+++ Day 60/functions.js
@@ -0,0 +1,18 @@
+//Task 1: Function Task
+
+//Task: Create a function called greet that takes a name as input and prints a greeting message like "Hello, [name]!".
+function greet(name) {
+    console.log("Assalamualykum, " + name + "!");
+  }
+  
+  greet("Amna");
+
+//   Task 2: Function Expression Task
+
+// Task: Create a function expression called double that doubles a given number.
+const double = function(num) {
+    return num * 2;
+  };
+  
+  console.log(double(5));
+  
