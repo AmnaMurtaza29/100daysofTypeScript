b4095fca75daa9d0f924f3f3a4c9195a612f770a
Amna Sadia Korai <murtazaamna117@gmail.com>
2024-05-05 18:20:37 +0500

    Day-61-of 100-Days-Of-Code Challenge

---
+++ Day 61/arrowfunctions.js
@@ -0,0 +1,10 @@
+let ask = (question, yes, no) => {
+    if (confirm(question)) yes();
+    else no();
+  }
+  ask(
+    "Do you agree?",
+    () => { alert("You agreed."); },
+    () => { alert("You canceled the execution."); }
+  );
+  
